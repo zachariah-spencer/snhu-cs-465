4cc3564e62ede5f3aa3b885c03a0e5c393a88b71
Zachariah <spencerzachariah@gmail.com>
2025-07-13 11:40:48 -0500

    build out mvc architecture

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./app_server/routes/index');
+var usersRouter = require('./app_server/routes/users');
+var travelRouter = require('./app_server/routes/travel');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+
+handlebars.registerPartials(__dirname + '/app_server/views/partials');
+
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
---
+++ app_server/controllers/travlr.js
@@ -0,0 +1,8 @@
+/* GET Homepage */
+const index = (req, res) => {
+    res.render('travel', { title: "Travlr Getaways"});
+};
+
+module.exports = {
+    travel
+}
