0be44afc83bf0ee37fe297869f129d6cce446f67
Zachariah <spencerzachariah@gmail.com>
2025-07-20 21:36:29 -0500

    fix view set in app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var travelRouter = require('./app_server/routes/travel');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/app_server/views'));
 
 handlebars.registerPartials(__dirname + '/app_server/views/partials');
 
