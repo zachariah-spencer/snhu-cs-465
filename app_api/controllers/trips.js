55e9ad4ecdce38cc076e8540898d0b4b3316c6da
Zachariah <spencerzachariah@gmail.com>
2025-08-03 13:16:37 -0500

    working on module 5

---
+++ app_api/controllers/trips.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Trip = require('../models/travlr'); // Register model
+const Model = mongoose.model('trips');
+
+// GET: /trips - lists all the trips
+// Regardless of outcomes, response must include HTML status code
+// and JSON message to the requesting client
+const tripsList = async(req, res) => {
+    const q = await Model.find({}).exec(); // No filtering, return all records
+    // Uncomment if debugging response
+    // console.log(q);
+
+    if (!q)
+    { // No data returned from database
+        return res
+            .status(404)
+            .json(err);
+    } else { // Return resulting trip list
+        return res
+            .status(200)
+            .json(q);
+    }
+};
+
+module.exports = {
+    tripsList
+};
---
+++ app_api/routes/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const tripsController = require("../controllers/trips");
+
+router.route("/trips").get(tripsController.tripsList);
+
+module.exports = router;
