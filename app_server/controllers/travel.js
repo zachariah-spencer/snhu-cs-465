719c4e7110e0b5a1618034baece5afb931bb8f0e
Zachariah <spencerzachariah@gmail.com>
2025-08-03 14:19:17 -0500

    build api endpoints and wire them up

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,4 +1,4 @@
-const tripsEndpoint = "https://localhost:3000/api/trips"
+const tripsEndpoint = "http://localhost:3000/api/trips"
 const options = {
     method: "GET",
     headers: {
@@ -14,6 +14,15 @@ const travel = async function (req, res, next) {
     await fetch(tripsEndpoint, options)
         .then((res) => res.json())
         .then((json) => {
+            let message = null;
+            if(!(json instanceof Array)) {
+                message = "API lookup error";
+                json = [];
+            } else {
+                if (!json.length) {
+                    message = "No trips exist in our database!";
+                }
+            }
             res.render("travel", { title: "Travlr Getaways", trips: json, message });
         })
         .catch((err) => res.status(500).send(err.message));
