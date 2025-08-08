fbc9db4474f5551407373e0dbe5de5a74ad8cdeb
Zachariah <spencerzachariah@gmail.com>
2025-08-08 18:31:40 -0500

    fix an error with routing syntax

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -4,7 +4,7 @@ const tripsController = require("../controllers/trips");
 
 router.route('/trips')
     .get(tripsController.tripsList) // GET Method routes tripsList
-    .post(tripsController.tripsAddtrip); // POST Method adds a trip
+    .post(tripsController.tripsAddTrip); // POST Method adds a trip
 
 router.route("/trips/:tripCode").get(tripsController.tripsFindByCode); // GET Method routes tripsFindBycode - requires a parameter
 
